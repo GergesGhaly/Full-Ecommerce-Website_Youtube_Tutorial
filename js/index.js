72e721f51c682c1acf5877bec048159d9e90787b
Gerges Ghaly <82944591+GergesGhaly@users.noreply.github.com>
2021-09-02 18:33:46 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const openNav = document.querySelector(".open-btn");
+const closeNav = document.querySelector(".close-btn");
+const menu = document.querySelector(".navlist");
+
+const menuLeft = menu.getBoundingClientRect().left;
+openNav.addEventListener("click", () => {
+  if (menuLeft < 0) {
+    menu.classList.add("show");
+  }
+});
+
+closeNav.addEventListener("click", () => {
+  if (menuLeft < 0) {
+    menu.classList.remove("show");
+  }
+});
+
+
+
